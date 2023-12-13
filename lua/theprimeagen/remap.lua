177eb6f5601b576dc6e83ea2607ff0b46e02c1a7
Lucas Nunzi <lucas.nunzi@life5.com>
2023-12-13 01:24:06 +0100

    more telescope

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -24,6 +24,7 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>P", "<cmd>Telescope registers<CR>")
 
     vim.keymap.set(
 	{"n", "x"},
