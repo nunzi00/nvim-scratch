21857c7d72eeba81c0e4be268f030024c64c5a36
Lucas Nunzi <lucas.nunzi@life5.com>
2023-12-14 21:38:49 +0100

    organize packer

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -7,12 +7,9 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use("nvim-lua/plenary.nvim")
-    use {
-        'nvim-telescope/telescope.nvim'
-        , tag = '0.1.4',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-
+    --
+    -- theme
+    --
     use({
         'rose-pine/neovim',
         as = 'rose-pine',
@@ -20,6 +17,16 @@ return require('packer').startup(function(use)
             vim.cmd('colorscheme rose-pine')
         end
     })
+
+    use { "catppuccin/nvim", as = "catppuccin" }
+    --
+    -- General
+    --
+    use {
+        'nvim-telescope/telescope.nvim'
+        , tag = '0.1.4',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
     use({
         "folke/trouble.nvim",
@@ -39,94 +46,23 @@ return require('packer').startup(function(use)
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
         end, }
-    use("theprimeagen/harpoon")
-    use("theprimeagen/refactoring.nvim")
-    use("mbbill/undotree")
-    use("tpope/vim-fugitive")
+
     use("nvim-treesitter/nvim-treesitter-context");
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+    use("theprimeagen/harpoon")
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
-
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-
-            -- Formatter
-            { "lukas-reineke/lsp-format.nvim" },
-        }
-    }
+    use("mbbill/undotree")
 
     use("folke/zen-mode.nvim")
+
     use("eandrju/cellular-automaton.nvim")
+
     use("laytan/cloak.nvim")
-    use("smjonas/inc-rename.nvim")
+
     use { "m4xshen/hardtime.nvim",
         requires = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     }
-    use { "nvim-neotest/neotest",
-        requires = {
-            "nvim-neotest/neotest-jest",
-            "olimorris/neotest-phpunit",
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "antoinemadec/FixCursorHold.nvim",
-            "folke/neodev.nvim"
-        },
-    }
-    use { "catppuccin/nvim", as = "catppuccin" }
----     use { "phpactor/phpactor" }
- use({
-  "gbprod/phpactor.nvim",
-  requires = {
-    "nvim-lua/plenary.nvim", -- required to update phpactor
-    "neovim/nvim-lspconfig" -- required to automaticly register lsp serveur
-  },
-  config = function()
-    require("phpactor").setup({
-    })
-  end
-})
-    use { "johmsalas/text-case.nvim",
-        config = function()
-            require('textcase').setup {}
-            require('telescope').load_extension('textcase')
-        end
-    }
-    use {
-        "ray-x/lsp_signature.nvim",
-        event = "BufRead",
-        config = function() require("lsp_signature").setup() end,
-    }
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
-    use { "folke/lsp-colors.nvim", config = function()
-        require("lsp-colors").setup {
-            Error       = "#db4b4b",
-            Warning     = "#e0af68",
-            Information = "#0db9d7",
-            Hint        = "#10B981"
-        }
-    end
-    }
+
     use {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
@@ -156,23 +92,7 @@ return require('packer').startup(function(use)
             },
         }
     }
-    use({
-        "kdheepak/lazygit.nvim",
-        -- optional for floating window border decoration
-        requires = {
-            "nvim-lua/plenary.nvim",
-        },
-    })
-    use {
-        'altermo/ultimate-autopair.nvim',
-        event = { 'InsertEnter', 'CmdlineEnter' },
-        branch = 'v0.6', --recomended as each new version will have breaking changes
-        config = function()
-            require('ultimate-autopair').setup({
-                --Config goes here
-            })
-        end,
-    }
+
     use {
         "folke/which-key.nvim",
         config = function()
@@ -185,19 +105,138 @@ return require('packer').startup(function(use)
             }
         end
     }
--- Status bar
+    --
+    -- LSP
+    --
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+
+            -- Formatter
+            { "lukas-reineke/lsp-format.nvim" },
+        }
+    }
+
+    use { "folke/lsp-colors.nvim", config = function()
+        require("lsp-colors").setup {
+            Error       = "#db4b4b",
+            Warning     = "#e0af68",
+            Information = "#0db9d7",
+            Hint        = "#10B981"
+        }
+    end
+    }
+
+    use("lukas-reineke/lsp-format.nvim")
+    --
+    -- Coding
+    --
+    use("smjonas/inc-rename.nvim")
+
+    use("theprimeagen/refactoring.nvim")
+
+    use("tpope/vim-fugitive")
+
+    use { "nvim-neotest/neotest",
+        requires = {
+            "nvim-neotest/neotest-jest",
+            "olimorris/neotest-phpunit",
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "antoinemadec/FixCursorHold.nvim",
+            "folke/neodev.nvim"
+        },
+    }
+
+    ---     use { "phpactor/phpactor" }
+    use({
+        "gbprod/phpactor.nvim",
+        requires = {
+            "nvim-lua/plenary.nvim", -- required to update phpactor
+            "neovim/nvim-lspconfig"  -- required to automaticly register lsp serveur
+        },
+        config = function()
+            require("phpactor").setup({
+            })
+        end
+    })
+
+    use { "johmsalas/text-case.nvim",
+        config = function()
+            require('textcase').setup {}
+            require('telescope').load_extension('textcase')
+        end
+    }
+
+    use {
+        "ray-x/lsp_signature.nvim",
+        event = "BufRead",
+        config = function() require("lsp_signature").setup() end,
+    }
+
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
+    use({
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
+    })
+
+    use {
+        'altermo/ultimate-autopair.nvim',
+        event = { 'InsertEnter', 'CmdlineEnter' },
+        branch = 'v0.6', --recomended as each new version will have breaking changes
+        config = function()
+            require('ultimate-autopair').setup({
+                --Config goes here
+            })
+        end,
+    }
+    --
+    -- Status bar
+    --
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
--- Session
+    --
+    -- Session
+    --
     use {
         'stevearc/resession.nvim',
         config = function() require('resession').setup() end
     }
--- Buffers
+    --
+    -- Buffers
+    --
     use { "moll/vim-bbye" }
--- Snips
+    --
+    -- Snipppets
+    --
     use({
         "L3MON4D3/LuaSnip",
         -- follow latest release.
@@ -205,6 +244,14 @@ return require('packer').startup(function(use)
         -- install jsregexp (optional!:).
         run = "make install_jsregexp"
     })
--- Markdown
-use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+    -- Markdown
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function()
+            vim.g.mkdp_filetypes = {
+                "markdown" }
+        end,
+        ft = { "markdown" },
+    })
 end)
