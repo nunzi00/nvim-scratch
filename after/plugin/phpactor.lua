f2638e596d7885d8382a3e27813f4432f70290e9
Lucas Nunzi <lucas.nunzi@life5.com>
2024-02-20 14:37:32 +0000

    phpactor

--- after/plugin/phpactor.lua
+++ after/plugin/phpactor.lua
@@ -3,24 +3,25 @@ local configs = require "lspconfig.configs"
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 configs.phpactor.setup {
-    default_config = {
-        enabled = true,
-        cmd = {
-            "phpactor",
-            "language-server",
+        filetypes = { 'php', 'feature' },
+        default_config = {
+                enabled = true,
+                cmd = {
+                        "phpactor",
+                        "language-server",
+                },
+                filetypes = { "php" },
+                root_dir = {
+                        "composer.json",
+                        ".git",
+                        "../composer.json",
+                        "../.git",
+                },
         },
-        filetypes = { "php" },
-        root_dir = {
-            "composer.json",
-            ".git",
-            "../composer.json",
-            "../.git",
-        },
-    },
 }
-vim.keymap.set("n", "<leader>u", ":call phpactor#UseAdd()<CR>") -- Include use statemen
-vim.keymap.set("n", "<leader>mm", ":call phpactor#ContextMenu()<CR>") -- Invoke the context menu
-vim.keymap.set("n", "<leader>nn", ":call phpactor#Navigate()<CR>") -- Invoke the navigation menu
+vim.keymap.set("n", "<leader>u", ":call phpactor#UseAdd()<CR>")                   -- Include use statemen
+vim.keymap.set("n", "<leader>mm", ":call phpactor#ContextMenu()<CR>")             -- Invoke the context menu
+vim.keymap.set("n", "<leader>nn", ":call phpactor#Navigate()<CR>")                -- Invoke the navigation menu
 vim.keymap.set("n", "<leader>ee", ":call phpactor#ExtractExpression()<CR>")
 vim.keymap.set("v", "<leader>ee", ":call phpactor#ExtractExpression(v:true)<CR>") -- Extract
 vim.keymap.set("v", "<leader>ee", ":call phpactor#ExtractExpression(v:true)<CR>")
